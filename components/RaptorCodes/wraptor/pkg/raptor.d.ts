4bb93c36a5113a5f8ca5464e33265932d7d1bee7
Orie Steele <orie@transmute.industries>
2023-12-29 12:11:11 -0600

    :rainbow::rocket:

---
+++ components/RaptorCodes/wraptor/pkg/raptor.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {Uint8Array} message
+* @returns {any}
+*/
+export function encode(message: Uint8Array): any;
+/**
+* @param {any} js_value
+* @returns {any}
+*/
+export function decode(js_value: any): any;
