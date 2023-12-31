dcfb2fc18e0d7b3ae046bc85b65cba0995167d6b
Orie Steele <orie@transmute.industries>
2023-12-31 11:42:54 -0600

    :rainbow::rocket:

---
+++ wraptor/pkg/raptor.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {any} js_value
+* @returns {any}
+*/
+export function encode(js_value: any): any;
+/**
+* @param {any} js_value
+* @returns {any}
+*/
+export function decode(js_value: any): any;
